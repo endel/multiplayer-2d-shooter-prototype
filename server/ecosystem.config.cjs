b6e80c53b372bb8e3f18ea14291d8cbce41a9521
Endel Dreyer <endel.dreyer@gmail.com>
2026-01-20 15:58:13 -0300

    update

--- server/ecosystem.config.cjs
+++ server/ecosystem.config.cjs
@@ -13,8 +13,10 @@ module.exports = {
     time: true,
     watch: false,
     instances: os.cpus().length,
-    exec_mode: 'fork',
-    wait_ready: true,
+    exec_mode: 'fork'
+    wait_ready: true
+
+
   }],
 };
 
